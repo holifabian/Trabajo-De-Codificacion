0d7d4a024e7715a4094f639dc08bb5e0d6a22d66
holifabian <116663438+holifabian@users.noreply.github.com>
2022-11-08 17:36:50 -0500

    Add files via upload

---
+++ src/PracticaPreevaluatoria.java
@@ -0,0 +1,8 @@
+public class PracticaPreevaluatoria {
+    public static void main(String[] args) {
+        System.out.println("PRUEBA DE OPERADORES LOGICOS O BOOLEANOS");
+        //boolean valor =
+
+
+    }
+}
