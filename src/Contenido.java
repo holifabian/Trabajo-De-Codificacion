930f1138ef6e74178bf516ec0c6cfec5a5e95af7
holifabian <116663438+holifabian@users.noreply.github.com>
2022-12-03 11:04:50 -0500

    Add files via upload

---
+++ src/Contenido.java
@@ -0,0 +1,24 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("Algoritmo: es básicamente un conjunto de pasos para una determinada tarea");
+        System.out.println("Bootstrap: framework CSS");
+        System.out.println("Código: conjunto de palabras o símbolos que contienen instrucciones para la computadora");
+        System.out.println("Queries: solicitud de información y/o datos para una base de datos.");
+        System.out.println("Full-stack: unión de Front-End y Back-End");
+        System.out.println("Backup: copia de archivos en otro dispositivo para guardar informacion");
+        System.out.println("Bandwidth: ancho de banda. Dice cuánto se puede viajar en una red");
+        System.out.println("Big Data: almacenamiento de grandes cantidades de datos");
+        System.out.println("Bit: abreviatura de binary digit");
+        System.out.println("Byte: unión de 8 bits");
+        System.out.println("Tipado: significa una variable o algún valor que tiene un tipo predeterminado");
+        System.out.println("Clase: proyecto de todo objeto");
+        System.out.println("Instancia: la ejecución de una clase");
+        System.out.println("Objeto: la ejecución de una clase");
+        System.out.println("Herencia: capacidad de heredar las características de otra clase");
+        System.out.println("Miembro de Clase: puede ser una función o una propiedad");
+        System.out.println("SuperClase: la clase padre; clase que se amplía");
+        System.out.println("SubClase: la clase hija; la clase que parte de la implementación de otra");
+        System.out.println("Dev. Team: equipo de desarrollo");
+        System.out.println("React: biblioteca JavaScript para crear el Front-End de aplicaciones");
+    }
+}
